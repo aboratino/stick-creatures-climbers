c86fe31e0d47c9539bf60c7291cd30bb23772859
aboratino <aboratino@gmail.com>
2019-04-12 10:48:28 -0700

    little tweaks

--- creature.py
+++ creature.py
@@ -13,7 +13,7 @@ RESET_X_MUL = 30        # how far out to place creature, RESET_X_MUL * cid
 
 CHANCE = 12             # 1 in CHANCE chance of mutation.
 
-N_SEG = 6               # number of segments per creature
+N_SEGMENTS = 6          # number of segments per creature
 
 
 # Creature class
@@ -21,7 +21,7 @@ class Creature:
     def __init__(self, cid, screen):
         self.cid = cid                  # creature id
         self.screen = screen
-        self.n_segments = N_SEG
+        self.n_segments = N_SEGMENTS
         self.limb = []
         self.is_stud = False
 
@@ -33,12 +33,11 @@ class Creature:
             self.limb[i].rot_rate = random()
 
         self.limb[0].is_first = True
-        self.limb[self.n_segments-1].is_last = True
+        self.limb[-1].is_last = True
         self.reset_location()
 
     # reset location to bottom of screen
     def reset_location(self):
-
         self.limb[0].bx = RESET_X_OFFSET + self.cid * RESET_X_MUL
         self.limb[0].by = RESET_Y
 
@@ -47,6 +46,12 @@ class Creature:
             self.limb[i].sforward = True
 
         self.update()
+
+    def new_ex(self, i):
+        return self.limb[i].length * math.sin(self.limb[i].rotation * (math.pi / 180.0))
+
+    def new_ey(self, i):
+        return self.limb[i].length * math.cos(self.limb[i].rotation * (math.pi / 180.0))
 
     # update location of segments
     def update(self):
@@ -58,21 +63,22 @@ class Creature:
                     self.limb[i].bx = self.limb[i-1].ex
                     self.limb[i].by = self.limb[i-1].ey
 
-                self.limb[i].ex = self.limb[i].bx+self.limb[i].length*math.sin(self.limb[i].rotation * (3.14 / 180.0))
-                self.limb[i].ey = self.limb[i].by+self.limb[i].length*math.cos(self.limb[i].rotation * (3.14 / 180.0))
+                self.limb[i].ex = self.limb[i].bx+self.new_ex(i)
+                self.limb[i].ey = self.limb[i].by+self.new_ey(i)
             else:
                 # if not last
                 if i < self.n_segments-1:
                     self.limb[i].ex = self.limb[i+1].bx
                     self.limb[i].ey = self.limb[i+1].by
 
-                self.limb[i].bx = self.limb[i].ex-self.limb[i].length*math.sin(self.limb[i].rotation * (3.14 / 180.0))
-                self.limb[i].by = self.limb[i].ey-self.limb[i].length*math.cos(self.limb[i].rotation * (3.14 / 180.0))
+                self.limb[i].bx = self.limb[i].ex-self.new_ex(i)
+                self.limb[i].by = self.limb[i].ey-self.new_ey(i)
 
     # draw all the segments
     def draw(self):
         if self.is_stud:
             self.limb[0].is_stud = True
+
         for i in range(self.n_segments):
             self.limb[i].draw()
 
@@ -88,17 +94,12 @@ class Creature:
     def get_loc(self):
         return self.limb[0].bx, self.limb[0].by
 
-    # breed with the winner
+    # breed: # for each segment blend and mutate with winner
     def breedwith(self, stud):
-
-        # for each segment blend and mutate
         for i in range(self.n_segments):
-            self.limb[i].counter = 0
-            stud.limb[i].counter = 0
+            self.limb[i].rotation = (self.limb[i].rotation +
+                                     stud.limb[i].grotation) / 2 + self.mutate()
+            self.limb[i].grotation = self.limb[i].rotation
 
-            self.limb[i].grotation = \
-                self.limb[i].rotation = \
-                (self.limb[i].rotation + stud.limb[i].grotation) / 2 + self.mutate()
             self.limb[i].rot_rate = (stud.limb[i].rot_rate +
                                      self.limb[i].rot_rate) / 2 + self.mutate()
-
